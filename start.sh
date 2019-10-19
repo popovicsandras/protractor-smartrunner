76a1cd7fde9d9d8ddf352f0d11cd8012ab75b6b2
Andras Popovics <popovics@ndras.hu>
2019-10-19 22:47:05 +0200

    Fix

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 npm run build
 npm run build:plugin
-./node_modules/lite-server/bin/lite-server --baseDir='./dist/testapp' >/dev/null & ./node_modules/protractor/bin/protractor ./e2e/protractor.conf.ts || exit 1
+./node_modules/lite-server/bin/lite-server --baseDir='./dist/testapp' -c ./lite-server.config.json >/dev/null & ./node_modules/protractor/bin/protractor ./e2e/protractor.conf.ts || exit 1

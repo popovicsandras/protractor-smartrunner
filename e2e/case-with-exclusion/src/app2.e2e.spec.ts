51f44d3b6acd7dc59c5f4f0c68cfc8c9e177325a
Andras Popovics <popovics@ndras.hu>
2021-03-03 16:46:41 +0100

    Handle too long file names

--- e2e/case-with-exclusion/src/app2.e2e.spec.ts
+++ e2e/case-with-exclusion/src/app2.e2e.spec.ts
@@ -2,14 +2,14 @@ import { AppPage } from '../../pages/app.po';
 import { browser, logging } from 'protractor';
 import { test } from '../../utils/utils';
 
-describe('Suite 2', () => {
+describe('Suite 2 super long name rorem ipsum dolor sit amet', () => {
     let page: AppPage;
 
     beforeEach(() => {
         page = new AppPage();
     });
 
-    describe('level 1a', () => {
+    describe('level 1a super long name rorem ipsum dolor sit amet', () => {
         const passForNthAttempt = [ 1, 1, 1, 1, 1, 2, 1, 1, 3, 1 ];
         for (let i = 0; i < 10; i++) {
             it(`should test scenario ${i}`, () => {
@@ -18,8 +18,8 @@ describe('Suite 2', () => {
         }
     });
 
-    describe('level 1b', () => {
-        describe('level 2', () => {
+    describe('level 1b super long name rorem ipsum dolor sit amet', () => {
+        describe('level 2 super long name rorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', () => {
             const passForNthAttempt = [ 1, 2, 1, 2, 1, 1, 1, 1, 1, 2 ];
             for (let i = 0; i < 10; i++) {
                 it(`should test scenario ${i}`, () => {
